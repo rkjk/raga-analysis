fef806102c30618e515b16a615783a6865c56658
Raghav Kuppan <raghav.kuppan@outlook.com>
2024-10-24 21:40:02 +0530

    Fix frame_length and hop_length

--- pitch-detect.py
+++ pitch-detect.py
@@ -33,16 +33,39 @@ CHANNELS = 1
 RATE = 44100
 CHUNK = int (0.020 * RATE) * 4  # 20 ms chunks x 4 bytes per sample
 
-
+class PYINPitchDetect:
+    def __init__(self, sr, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), frame_length=None, hop_length=None):
+        if not sr:
+            raise RuntimeError("Sample rate not specified")
+        if not frame_length:
+            frame_length = 1024
+        self.sr = sr
+        self.fmin = fmin
+        self.fmax = fmax
+        self.frame_length = frame_length
+        self.hop_length = hop_length
+    
+    def detect(self, data):
+        return librosa.pyin(
+                data,
+                fmin=self.fmin, 
+                fmax=self.fmax,
+                sr=self.sr,
+                frame_length=self.frame_length,
+                hop_length=self.hop_length)
+    
+    def name(self):
+        return "PYIN"
 
 class LiveAudioDetect:
-    def __init__(self, forma, channels, rate, frames_per_buffer):
+    def __init__(self, forma, channels, rate, frames_per_buffer, pitch_detector):
         self.format = forma
         self.channels = channels
         self.rate = rate
         self.frames_per_buffer = frames_per_buffer
         self.initialize()
         self.bag_of_notes = defaultdict(set)
+        self.pitch_detector = pitch_detector
         
     def __del__(self):
         # Close the audio stream
@@ -67,18 +90,12 @@ class LiveAudioDetect:
             frames_per_buffer=self.frames_per_buffer)
         print("Starting real-time pitch detection...")
         while True:
-        # Read audio data from the stream - We need 10 frames per second
+        # Read audio data from the stream - We need roughly frames per second
         # we get rate samples per second
-
-            data = np.frombuffer(stream.read(CHUNK), dtype=np.float32)
+            data = np.frombuffer(stream.read(self.rate), dtype=np.float32)
         
             # Detect pitch using the PYin algorithm
-            ff, voiced_flag, voiced_prob = librosa.pyin(
-                data,
-                fmin = librosa.note_to_hz('A2'), 
-                fmax=librosa.note_to_hz('G#5'), 
-                sr=RATE,
-                frame_length=CHUNK)
+            ff, voiced_flag, voiced_prob = self.pitch_detector.detect(data)
 
             # Print the detected pitch
             for k in range(len(ff)):
@@ -93,13 +110,16 @@ class LiveAudioDetect:
 
         stream.stop_stream()
         stream.close()
-        p.terminate()
+        self.p.terminate()
 
 if __name__ == "__main__":
     # Set up audio parameters
     FORMAT = pyaudio.paFloat32
     CHANNELS = 1
     RATE = 44100
+    FRAME_LENGTH = 2048
+    HOP_LENGTH = 512
     CHUNK = int (0.020 * RATE) * 4  # 20 ms chunks x 4 bytes per sample
-    detect = LiveAudioDetect(FORMAT, CHANNELS, RATE, CHUNK)
+    pyin_pitch_detector = PYINPitchDetect(RATE, frame_length=2048, hop_length=512)
+    detect = LiveAudioDetect(FORMAT, CHANNELS, RATE, CHUNK, pyin_pitch_detector)
     detect.start()
